8b22733cf36ea1de00e0613039064d033eb9f0a1
Ryan Yogan <ryanyogan@gmail.com>
2023-06-15 12:53:40 -0500

    header wip

--- src/app/components/navbar/user-menu.tsx
+++ src/app/components/navbar/user-menu.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../avatar";
+import MenuItem from "./menu-item";
 
 export default function UserMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -15,7 +23,7 @@ export default function UserMenu() {
         </div>
 
         <div
-          onClick={() => {}}
+          onClick={toggleIsOpen}
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
@@ -24,6 +32,16 @@ export default function UserMenu() {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign up" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
