f303088276d8ea32353950ae71792861a0655d00
Ryan Yogan <ryanyogan@gmail.com>
2023-06-17 21:16:54 -0500

    listing page looks better

--- src/app/components/listings/listing-client.tsx
+++ src/app/components/listings/listing-client.tsx
@@ -6,6 +6,7 @@ import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
 import { useMemo } from "react";
 import ListingHead from "./listing-head";
+import ListingInfo from "./listing-info";
 
 interface ListingClientProps {
   reserverations?: Reservation[];
@@ -34,6 +35,18 @@ export default function ListingClient({
             id={listing.id}
             currentUser={currentUser}
           />
+
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
